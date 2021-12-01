b464d27f79d33d40b1dea8dcef1601ede4c24b3c
Maksim Penza <w_max_hot_e@mail.ru>
2021-12-01 17:29:00 +0300

    01.12.21

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,13 +3,38 @@ package jm.task.core.jdbc.util;
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
+import java.util.Properties;
 
 public class Util {
 
-
-    //метод в задаче
+    private static final String Url = "jdbc:mysql://localhost/";
+    private static final String Username = "Max";
+    private static final String Password = "1111";
+    private static final String Driver = "com.mysql.cj.jdbc.Driver";
     private static SessionFactory sessionFactory;
 
+
+
+    public static SessionFactory getSessionFactory() {
+ try {
+        Properties settings = new Properties();
+        settings.put(Environment.DRIVER, Driver);
+        settings.put(Environment.URL, Url);
+        settings.put(Environment.USER, Username);
+        settings.put(Environment.PASS, Password);
+        settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(Environment.SHOW_SQL, "true");
+        settings.put(Environment.HBM2DDL_AUTO, "create-drop");
+        sessionFactory = new Configuration().setProperties(settings).addAnnotatedClass(User.class).buildSessionFactory();
+    } catch (Exception exception) {
+        throw new ExceptionInInitializerError();
+
+    } return sessionFactory;
+}
+
+    /*
     public static SessionFactory getSessionFactory() {
         try {
             sessionFactory = new Configuration().addAnnotatedClass(User.class).buildSessionFactory();
@@ -21,6 +46,8 @@ public class Util {
         }
         return sessionFactory;
     }
+
+     */
 
 
 
